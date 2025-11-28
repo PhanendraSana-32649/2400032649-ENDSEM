67e62280ad53a56437c246d4b7af60e16f0831a9
Sana Phanendra <2400032649@kluniversity.in>
2025-11-28 17:54:53 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import EventBooking from "./pages/EventBooking";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>Event Booking Portal</h1>
+      <EventBooking />
+    </div>
+  );
+};
+
+export default App;
